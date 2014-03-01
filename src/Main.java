4912ebb24ab1d0a5cb82e6dc9531a9ccf61f220b
Austin Beeler <abeeler1@uncc.edu>
2014-03-01 12:55:37 -0500

    Buttons moved to their own class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 import rubide.*;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -15,37 +11,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        HBox layout = new HBox();
-        
-        VBox buttons = new VBox();
-        
-        
-        Button save = new Button("Save");
-        Button open = new Button("Open");
-        
-        buttons.getChildren().addAll(save, open);
+        HBox layout = new HBox();             
         
         final FileTabPane files = new FileTabPane();
         
+        ButtonLayout buttons = new ButtonLayout(files);
+        
         layout.getChildren().addAll(files, buttons);
       
-        Scene scene = new Scene(layout, 551, 400);
-        
-        save.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-            public void handle(ActionEvent e) {
-        	 	files.save();
-            }
-        });
-        
-        open.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-            public void handle(ActionEvent e) {
-               files.openFiles();
-            }        	
-        });
-        
-        // Dropping over surface
+        Scene scene = new Scene(layout, 551, 400);       
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
---
+++ src/rubide/ButtonLayout.java
@@ -0,0 +1,38 @@
+package rubide;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class ButtonLayout extends VBox {
+	
+	final FileTabPane files;
+	
+	public ButtonLayout(FileTabPane filePane) {
+		this.files = filePane;
+		
+        Button save = new Button("Save");
+        Button open = new Button("Open");
+        
+        addButtons(save, open);
+		
+        save.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+            public void handle(ActionEvent e) {
+        		files.save();
+            }
+        });
+        
+        open.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+            public void handle(ActionEvent e) {
+        		files.openFiles();
+            }        	
+        });
+	}
+	
+	public void addButtons(Button... btns) {
+		this.getChildren().addAll(btns);
+	}
+}
